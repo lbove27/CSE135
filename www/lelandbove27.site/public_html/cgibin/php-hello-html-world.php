05eb5bd7279bf7af70579b5b68f2d21205e2b799
lbove27 <lbove@ucsd.edu>
2023-04-23 10:45:01 -0700

    php hello world

---
+++ www/lelandbove27.site/public_html/cgibin/php-hello-html-world.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <h1>PHP Hello World HTML</h1>
+    <?php
+        echo "Hello World\n";
+        echo date();
+        echo 'User IP Address' $_SERVER['REMOTE_ADDR'];
+    ?>
+</body>
+</html>
