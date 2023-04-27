32e5201d494d8f4e3ef1957255e93feafd4791dc
lbove27 <lbove@ucsd.edu>
2023-04-27 15:03:54 -0700

    try using session_start

--- www/lelandbove27.site/public_html/cgibin/php-state-demo-1.php
+++ www/lelandbove27.site/public_html/cgibin/php-state-demo-1.php
@@ -5,8 +5,17 @@
     print "</head>";
     print "<body>";
     print "<h1>PHP Sessions Page 1</h1>";
-    print "<p>Current Session Value: " . $_POST['session-var'] . "</p>";
-    setcookie("sessionCookie", $_POST['session-var']);
+    if($_POST['session-var'] == null) {
+        print "<p>Current Session Value: " . $_SESSION['session-var'] . "</p>";
+    }
+    else if($_POST['session-var'] == null && $_SESSION['session-var'] == null) {
+        print "<p>No value is currently set</p>";
+    }
+    else {
+        print "<p>Current Session Value: " . $_POST['session-var'] . "</p>";
+    }
+    session_name('session-var');
+    session_start();
     print "<a href='php-state-demo-2.php'>Page 2</a>";
     print "</body>";
     print "</html>";
