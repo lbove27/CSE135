a319aa02ac15537776c570f3da0b0c853763c82f
lbove27 <lbove@ucsd.edu>
2023-04-27 08:45:44 -0700

    actually fixed it

--- www/lelandbove27.site/public_html/cgibin/php-state-demo-1.php
+++ www/lelandbove27.site/public_html/cgibin/php-state-demo-1.php
@@ -6,7 +6,7 @@
     print "<body>";
     print "<h1>PHP Sessions Page 1</h1>";
     print "<p>Current Session Value: " . $_GET['session-var'] . "</p>";
-    setcookie("sessionCookie", $_GET['sessionVal']);
+    setcookie("sessionCookie", $_GET['session-var']);
     print "<a href='php-state-demo-2.php'>Page 2</a>";
     print "</body>";
     print "</html>";
