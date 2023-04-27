be27cb3ddfae4d619b66246ac32c9d8428abd928
lbove27 <lbove@ucsd.edu>
2023-04-27 08:51:37 -0700

    changed to a post instead

--- www/lelandbove27.site/public_html/cgibin/php-state-demo-1.php
+++ www/lelandbove27.site/public_html/cgibin/php-state-demo-1.php
@@ -5,8 +5,8 @@
     print "</head>";
     print "<body>";
     print "<h1>PHP Sessions Page 1</h1>";
-    print "<p>Current Session Value: " . $_GET['session-var'] . "</p>";
-    setcookie("sessionCookie", $_GET['session-var']);
+    print "<p>Current Session Value: " . $_POST['session-var'] . "</p>";
+    setcookie("sessionCookie", $_POST['session-var']);
     print "<a href='php-state-demo-2.php'>Page 2</a>";
     print "</body>";
     print "</html>";
