80c050c37442e16b26409c5274bc1537c4d20eea
lbove27 <lbove@ucsd.edu>
2023-04-28 10:44:30 -0700

    fix date

--- www/lelandbove27.site/public_html/cgibin/python-hello-html-world.py
+++ www/lelandbove27.site/public_html/cgibin/python-hello-html-world.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from datetime import date;
+from datetime import datetime;
 
 print ("Content-type:text/html\n")
 print ('<html>')
@@ -12,7 +12,8 @@ print ('<h1>Python Hello HTML World</h1>')
 print ('<hr>')
 print ('<p>Hello World</p>')
 print ('<p>This page was generated with the Python Programming Language</p>')
-print ('<p>This program was run at: ' + date.today() + '</p>')
+datetime_str = datetime.now.strftime("%d/%m/%Y %H:%M:%S")
+print ('<p>This program was run at: ' + datetime_str + '</p>')
 print ('<p>Your current IP address is: ' + '')
 print ('</body>')
 print ('</html>')
