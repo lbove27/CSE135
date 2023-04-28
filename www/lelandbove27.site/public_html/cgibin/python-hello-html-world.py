6d303c2f3db855c6e8a1b767b0377fcbc77df90e
lbove27 <lbove@ucsd.edu>
2023-04-28 10:51:00 -0700

    do semicolons matter?

--- www/lelandbove27.site/public_html/cgibin/python-hello-html-world.py
+++ www/lelandbove27.site/public_html/cgibin/python-hello-html-world.py
@@ -2,18 +2,18 @@
 
 from datetime import datetime;
 
-print ("Content-type:text/html\n")
-print ('<html>')
-print ('<head>')
-print ('<title>Python Hello HTML World</title>')
-print ('</head>')
-print ('<body>')
-print ('<h1>Python Hello HTML World</h1>')
-print ('<hr>')
-print ('<p>Hello World</p>')
-print ('<p>This page was generated with the Python programming language</p>')
-datetime_str = datetime.now.strftime("%d/%m/%Y %H:%M:%S")
-print ('<p>This program was run at: ' + datetime_str + '</p>')
-print ('<p>Your current IP address is: ' + '') 
-print ('</body>')
-print ('</html>')
+print ("Content-type:text/html\n");
+print ('<html>');
+print ('<head>');
+print ('<title>Python Hello HTML World</title>');
+print ('</head>');
+print ('<body>');
+print ('<h1>Python Hello HTML World</h1>');
+print ('<hr>');
+print ('<p>Hello World</p>');
+print ('<p>This page was generated with the Python programming language</p>');
+datetime_str = datetime.now.strftime("%d/%m/%Y %H:%M:%S");
+print ('<p>This program was run at: ' + datetime_str + '</p>');
+print ('<p>Your current IP address is: ' + '');
+print ('</body>');
+print ('</html>');
