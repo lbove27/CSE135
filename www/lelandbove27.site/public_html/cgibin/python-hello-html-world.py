3126d38208e2434ca2a18939a2e1723ec1f4f34b
lbove27 <lbove@ucsd.edu>
2023-04-28 14:36:30 -0700

    update python

--- www/lelandbove27.site/public_html/cgibin/python-hello-html-world.py
+++ www/lelandbove27.site/public_html/cgibin/python-hello-html-world.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import datetime;
 import socket;
-
+import requests;
+from flask import request; 
+ 
 print ("Content-type:text/html\n");
 print ('<html>');
 print ('<head>');
@@ -13,10 +15,11 @@ print ('<h1>Python Hello HTML World</h1>');
 print ('<hr>');
 print ('<p>Hello World</p>');
 print ('<p>This page was generated with the Python programming language</p>');
-datetime_str = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S");
+datetime_str = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S");
 print ('<p>This program was run at: ' + datetime_str + '</p>');
 name = socket.gethostname();
 ip = socket.gethostbyname(name);
+print (request.environ.get('HTTP_X_REAL_IP', request.remote_addr));
 print ('<p>Your current IP address is: ' + ip);
 print ('</body>');
 print ('</html>');
