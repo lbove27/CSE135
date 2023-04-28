51e9cba429d01f7632acaf413a950c267f382509
lbove27 <lbove@ucsd.edu>
2023-04-28 10:56:51 -0700

    remove code

--- www/lelandbove27.site/public_html/cgibin/python-hello-html-world.py
+++ www/lelandbove27.site/public_html/cgibin/python-hello-html-world.py
@@ -13,8 +13,9 @@ print ('<h1>Python Hello HTML World</h1>');
 print ('<hr>');
 print ('<p>Hello World</p>');
 print ('<p>This page was generated with the Python programming language</p>');
-datetime_str = datetime.now.strftime("%d/%m/%Y %H:%M:%S");
-print ('<p>This program was run at: ' + datetime_str + '</p>');
+#datetime_str = datetime.now.strftime("%d/%m/%Y %H:%M:%S");
+#datetime_str + 
+print ('<p>This program was run at: ' + '' + '</p>');
 print ('<p>Your current IP address is: ' + '');
 print ('</body>');
 print ('</html>');
