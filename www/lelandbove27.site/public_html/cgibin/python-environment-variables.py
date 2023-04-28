c6ffb7c1baadac50b6cf3d1b958568aac2f72d13
lbove27 <lbove@ucsd.edu>
2023-04-28 15:41:06 -0700

    environment variables

---
+++ www/lelandbove27.site/public_html/cgibin/python-environment-variables.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os;
+ 
+print ("Content-type:text/html\n");
+print ('<html>');
+print ('<head>');
+print ('<title>Python Environment Variables</title>');
+print ('</head>');
+print ('<body>');
+print ('<h1>Python Environment Variables</h1>'); 
+print ('<hr>');
+print ('<h2>Environment Variables: ');
+print ('<ul>');
+for key, value in os.environ.items():
+    print('<li> <b>{k}:</b> {value}');
+print ('</ul>');
+print ('</body>');
+print ('</html>');
