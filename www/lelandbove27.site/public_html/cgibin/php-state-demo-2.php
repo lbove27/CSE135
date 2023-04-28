7bd1c12186fc8e505bdbeb1d67b022fee9ef270c
lbove27 <lbove@ucsd.edu>
2023-04-27 18:02:23 -0700

    fix destroy

--- www/lelandbove27.site/public_html/cgibin/php-state-demo-2.php
+++ www/lelandbove27.site/public_html/cgibin/php-state-demo-2.php
@@ -9,8 +9,11 @@
     session_name('session-var');
     session_start();
     print "<p>Current session value: " . $_SESSION["session-var"] . "</p>";
-    print "<a href='./php-state-demo-1.php'>Page 1</a>";
-    print "<a href='/forms/php-state-demo.html'>Back to form</a>";
+    print "<a href='./php-state-demo-1.php'>Page 1</a>\n";
+    print "<a href='../forms/php-state-demo.html'>Back to form</a>\n";
+    print "<form action='./php-state-destroy.php' method=get>";
+    print "<button type='submit' id='destroy-session'>Destroy session</button>";
+    print "</form>";
     print "</body>";
     print "</html>";
 ?>
--- www/lelandbove27.site/public_html/cgibin/php-state-destroy.php
+++ www/lelandbove27.site/public_html/cgibin/php-state-destroy.php
@@ -7,7 +7,7 @@
     print "<h1>PHP Sessions Page 1</h1>";
     session_name('session-var');
     session_destroy();
-    print "<a href='forms/php-state-demo.html'>Back to form</a>";
+    print "<a href='../forms/php-state-demo.html'>Back to form</a>";
     print "<a href='php-state-demo-1.php'>Page 1</a>";
     print "<a href='php-state-demo-2.php'>Page 2</a>";
     print "</body>";
