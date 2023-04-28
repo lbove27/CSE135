15d55eaf02f771a09843cfd385f8e9904050bb03
lbove27 <lbove@ucsd.edu>
2023-04-28 11:42:26 -0700

    pyhton hello world

--- www/lelandbove27.site/public_html/cgibin/python-hello-html-world.py
+++ www/lelandbove27.site/public_html/cgibin/python-hello-html-world.py
@@ -9,7 +9,7 @@ print ('<head>');
 print ('<title>Python Hello HTML World</title>');
 print ('</head>');
 print ('<body>');
-print ('<h1>Python Hello HTML World</h1>');
+print ('<h1>Python Hello HTML World</h1>'); 
 print ('<hr>');
 print ('<p>Hello World</p>');
 print ('<p>This page was generated with the Python programming language</p>');
