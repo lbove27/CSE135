ab3b71c13c51dadf6f7f15ec9578e54e397aea69
lbove27 <lbove@ucsd.edu>
2023-04-28 17:14:45 -0700

    fixed get echo

--- www/lelandbove27.site/public_html/cgibin/python-get-echo.py
+++ www/lelandbove27.site/public_html/cgibin/python-get-echo.py
@@ -11,8 +11,17 @@ print ('<body>');
 print ('<h1>Python Environment Variables</h1>'); 
 print ('<hr>');
 print ('<h2>Environment Variables: </h2>');
+queryStr = os.environ['QUERY_STRING']
+print ('<p>Query String: ' + queryStr + '</p>');
 print ('<ul>');
-print ('<p>Query String: ' + os.environ['QUERY_STRING'] + '</p>');
+params = queryStr.split('&');
+for param in params: 
+    keyValuePair = param.split('=');
+    values = keyValuePair[1].split('+');
+    finalVal = '';
+    for val in values:
+        finalVal += (val + ' ');  
+    print('<li><b>' + keyValuePair[0] + '</b>: ' + finalVal);
 print ('</ul>');       
 print ('</body>');
 print ('</html>');
