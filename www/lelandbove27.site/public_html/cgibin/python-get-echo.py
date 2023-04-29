aa8e64728c30f4f9c1a76aed0d113e73a13cad5d
lbove27 <lbove@ucsd.edu>
2023-04-28 17:21:12 -0700

    update

--- www/lelandbove27.site/public_html/cgibin/python-get-echo.py
+++ www/lelandbove27.site/public_html/cgibin/python-get-echo.py
@@ -5,12 +5,11 @@ import os;
 print ("Content-type:text/html\n");
 print ('<html>');
 print ('<head>');
-print ('<title>Python Environment Variables</title>');
+print ('<title>Python GET Echo</title>');
 print ('</head>');
 print ('<body>');
-print ('<h1>Python Environment Variables</h1>'); 
+print ('<h1>Python GET Echo</h1>'); 
 print ('<hr>');
-print ('<h2>Environment Variables: </h2>');
 queryStr = os.environ['QUERY_STRING']
 print ('<p>Query String: ' + queryStr + '</p>');
 print ('<ul>');
---
+++ www/lelandbove27.site/public_html/cgibin/python-post-echo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os;
+ 
+print ("Content-type:text/html\n");
+print ('<html>');
+print ('<head>');
+print ('<title>Python GET Echo</title>');
+print ('</head>');
+print ('<body>');
+print ('<h1>Python GET Echo</h1>'); 
+print ('<hr>');
+#queryStr = os.environ['MESSAGE_BODY'];
+for key, value in os.environ.items():
+    print ('<li><b>' + key + ': </b>' + value + '</li>');
+print ('<ul>');
+#params = queryStr.split('&');
+#for param in params: 
+    #keyValuePair = param.split('=');
+    #values = keyValuePair[1].split('+');
+    #finalVal = '';
+    #for val in values:
+        #finalVal += (val + ' ');  
+    #print('<li><b>' + keyValuePair[0] + '</b>: ' + finalVal);
+print ('</ul>');       
+print ('</body>');
+print ('</html>');
