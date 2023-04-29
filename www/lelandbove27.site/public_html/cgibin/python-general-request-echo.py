a61c540cb5fdeef12a695d97c02c3722d041e2d6
lbove27 <lbove@ucsd.edu>
2023-04-29 09:55:54 -0700

    done with general request

---
+++ www/lelandbove27.site/public_html/cgibin/python-general-request-echo.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import sys;
+import os;
+ 
+print ("Content-type:text/html\n");
+print ('<html>');
+print ('<head>');
+print ('<title>Python General Request Echo</title>');
+print ('</head>');
+print ('<body>');
+print ('<h1>Python General Request Echo</h1>'); 
+print ('<hr>');
+print ('<p>Request Method: ' + os.environ['REQUEST_METHOD'] + '</p>');
+print ('<p>Protocol: ' + os.environ['SERVER_PROTOCOL'] + '</p>');
+print ('<hr>');
+queryStr = os.environ['QUERY_STRING']
+print ('<p>Query String: ' + queryStr + '</p>');
+print ('<ul>');
+params = queryStr.split('&');
+for param in params: 
+    keyValuePair = param.split('=');
+    values = keyValuePair[1].split('+');
+    finalVal = '';
+    for val in values:
+        finalVal += (val + ' ');  
+    print('<li><b>' + keyValuePair[0] + '</b>: ' + finalVal);
+print ('</ul>');
+print ('<hr>');
+print ('<ul>');
+data = sys.stdin.read();
+value = data.split('=');
+values = value[1].split('+');
+data_str = '';
+for word in values:
+    data_str += (word + ' ');
+print ('<li><b>Message body: </b>' + data_str + '</li>');
+print ('</ul>');       
+print ('</body>');
+print ('</html>');
