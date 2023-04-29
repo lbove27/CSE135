00b9b6e008e7435c24d530dd8da79c3243941245
lbove27 <lbove@ucsd.edu>
2023-04-28 17:04:56 -0700

    hi

--- www/lelandbove27.site/public_html/cgibin/python-get-echo.py
+++ www/lelandbove27.site/public_html/cgibin/python-get-echo.py
@@ -1,3 +1,18 @@
-import os;
+#!/usr/bin/env python3
 
-print(os.environ["HTTP_X_CLIENT_VERSION"]);
+import os;
+ 
+print ("Content-type:text/html\n");
+print ('<html>');
+print ('<head>');
+print ('<title>Python Environment Variables</title>');
+print ('</head>');
+print ('<body>');
+print ('<h1>Python Environment Variables</h1>'); 
+print ('<hr>');
+print ('<h2>Environment Variables: </h2>');
+print ('<ul>');
+print ('<p>Query String: ' + os.environ['QUERY_STRING'] + '</p>');
+print ('</ul>');       
+print ('</body>');
+print ('</html>');
