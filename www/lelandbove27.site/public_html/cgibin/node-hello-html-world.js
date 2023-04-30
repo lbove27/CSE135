17eb3a756ec2089c0b8dfc175356503f7cf1d102
lbove27 <lbove@ucsd.edu>
2023-04-30 15:54:29 -0700

    first html

--- www/lelandbove27.site/public_html/cgibin/node-hello-html-world.js
+++ www/lelandbove27.site/public_html/cgibin/node-hello-html-world.js
@@ -1,13 +1,5 @@
 #!/usr/bin/env node
 
 process.stdout.write("Content-Type: text/plain\r\n\r\n");
-process.stdout.write("Hello, world");
-/*
-var http = require('http');
+process.stdout.write("<h1>Hello, world</h1>");
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World');
-}).listen(3000, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:3000/');
-*/
