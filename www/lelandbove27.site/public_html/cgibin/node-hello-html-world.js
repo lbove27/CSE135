e47203aba0f7db2c5b3ce3e5ea3f381642df8e6f
lbove27 <lbove@ucsd.edu>
2023-04-30 15:54:46 -0700

    actual html now

--- www/lelandbove27.site/public_html/cgibin/node-hello-html-world.js
+++ www/lelandbove27.site/public_html/cgibin/node-hello-html-world.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-process.stdout.write("Content-Type: text/plain\r\n\r\n");
+process.stdout.write("Content-Type: text/html\r\n\r\n");
 process.stdout.write("<h1>Hello, world</h1>");
 
