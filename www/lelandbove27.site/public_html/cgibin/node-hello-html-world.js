99b48722c7912380620ab23ee8e8dc923d90b6ba
lbove27 <lbove@ucsd.edu>
2023-04-30 16:23:24 -0700

    op address node

--- www/lelandbove27.site/public_html/cgibin/node-hello-html-world.js
+++ www/lelandbove27.site/public_html/cgibin/node-hello-html-world.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 
 process.stdout.write("Content-Type: text/html\r\n\r\n");
-process.stdout.write("<h1>Hello, world</h1>");
+process.stdout.write("<!DOCTYPE html>");
+process.stdout.write("<html lang='en'>");
+process.stdout.write("<head>");
+process.stdout.write("<title>Node Hello HTML World</title>");
+process.stdout.write("</head>");
+process.stdout.write("<body>");
+process.stdout.write("<h1>Node Hello HTML World</h1>");
+process.stdout.write("<p>Hello World!</p>");
+process.stdout.write("<p>IP address: " + process.env.IP + "</p>");
+process.stdout.write("</body>");
+process.stdout.write("</html>");
 
