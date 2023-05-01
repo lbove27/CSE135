9f9ac85f1a6a6c7c05b45ee1970bbff7898e5859
lbove27 <lbove@ucsd.edu>
2023-04-30 21:22:12 -0700

    finish general request

---
+++ www/lelandbove27.site/public_html/cgibin/node-general-request-echo.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const querystring = require('node:querystring'); 
+const { appendFile } = require('node:fs'); 
+var fs = require("fs");
+
+process.stdout.write("Content-Type: text/html\r\n\r\n");
+process.stdout.write("<!DOCTYPE html>");
+process.stdout.write("<html lang='en'>");
+process.stdout.write("<head>");
+process.stdout.write("<title>Node General Request Echo</title>");
+process.stdout.write("</head>");
+process.stdout.write("<body>");
+process.stdout.write("<h1>Node General Reqeust Echo</h1>");
+process.stdout.write("<hr>");
+process.stdout.write("<p>Request Method: " + process.env.REQUEST_METHOD + "</p>");
+process.stdout.write("<p>Protocol: " + process.env.SERVER_PROTOCOL + "</p>");
+qStr = process.env.QUERY_STRING;
+process.stdout.write("<p><b>Query string: </b>" + qStr + "</p>");
+process.stdout.write("<ul>");
+values = querystring.parse(qStr);
+for(let val in values) {
+    process.stdout.write("<li><b>" + val + ":</b> " + values[val] + "</li>");
+}
+process.stdout.write("</ul>");
+process.stdout.write("<hr>");
+process.stdout.write("<ul>");
+let stdin = fs.readFileSync(0).toString();
+process.stdout.write("<p><b>Message Body: </b></p>");
+process.stdout.write("<ul>");
+postvalues = querystring.parse(stdin);
+for(let val in postvalues) {
+    process.stdout.write("<li><b>" + val + ":</b> " + values[val] + "</li>");
+}
+process.stdout.write("</ul>");
+process.stdout.write("</body>");
+process.stdout.write("</html>");
--- www/lelandbove27.site/public_html/cgibin/node-post-echo.js
+++ www/lelandbove27.site/public_html/cgibin/node-post-echo.js
@@ -13,7 +13,6 @@ process.stdout.write("</head>");
 process.stdout.write("<body>");
 process.stdout.write("<h1>Node POST Echo</h1>");
 process.stdout.write("<hr>");
-//messageBody = process.stdin.read();
 let stdin = fs.readFileSync(0).toString();
 process.stdout.write("<p><b>Message Body: </b></p>");
 process.stdout.write("<ul>");
