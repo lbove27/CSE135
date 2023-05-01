906ece370cb80ab182bddd5ee808fb37b963483f
lbove27 <lbove@ucsd.edu>
2023-04-30 18:15:21 -0700

    fixed date

--- www/lelandbove27.site/public_html/cgibin/node-hello-html-world.js
+++ www/lelandbove27.site/public_html/cgibin/node-hello-html-world.js
@@ -10,7 +10,7 @@ process.stdout.write("<body>");
 process.stdout.write("<h1>Node Hello HTML World</h1>");
 process.stdout.write("<p>Hello World!</p>");
 process.stdout.write("<p>This page was generated with Node.js (no express)</p>");
-process.stdout.write("<p>This program was run at: " + Date.now() + "</p>");
+process.stdout.write("<p>This program was run at: " + Date() + "</p>");
 process.stdout.write("<p>Your current IP address is " + process.env.REMOTE_ADDR + "</p>");
 process.stdout.write("</body>");
 process.stdout.write("</html>");
