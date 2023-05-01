63f9d6a022749a113aa1b057e7b56da19e2b9433
lbove27 <lbove@ucsd.edu>
2023-04-30 18:39:51 -0700

    fixed php html

--- www/lelandbove27.site/public_html/cgibin/php-hello-html-world.php
+++ www/lelandbove27.site/public_html/cgibin/php-hello-html-world.php
@@ -4,8 +4,10 @@
 <body>
     <?php
         print "<h1>PHP Hello World HTML</h1>";
-        print "<p>Hello World</p>\n";
-        print "<p>Date: " . date("jS F Y h:i:s") . "</p>";
+        print "<hr>";
+        print "<p>Hello World</p>";
+        print "<p>This page was generated using PHP.</p>";
+        print "<p>This program was run at: " . date("jS F Y h:i:s") . "</p>";
         print "<p>User IP Address: " . $_SERVER["REMOTE_ADDR"] . "</p>";
     ?>
 </body>
