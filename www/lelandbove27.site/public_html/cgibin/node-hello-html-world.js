2f68f22ee04c9b8f1f0d9b82a10869190212d3fb
lbove27 <lbove@ucsd.edu>
2023-04-30 18:13:50 -0700

    finished node hello html

--- www/lelandbove27.site/public_html/cgibin/node-hello-html-world.js
+++ www/lelandbove27.site/public_html/cgibin/node-hello-html-world.js
@@ -9,8 +9,9 @@ process.stdout.write("</head>");
 process.stdout.write("<body>");
 process.stdout.write("<h1>Node Hello HTML World</h1>");
 process.stdout.write("<p>Hello World!</p>");
-process.stdout.write("<p>IP address: " + process.env + "</p>");
-console.log(process.env);
+process.stdout.write("<p>This page was generated with Node.js (no express)</p>");
+process.stdout.write("<p>This program was run at: " + Date.now() + "</p>");
+process.stdout.write("<p>Your current IP address is " + process.env.REMOTE_ADDR + "</p>");
 process.stdout.write("</body>");
 process.stdout.write("</html>");
 
