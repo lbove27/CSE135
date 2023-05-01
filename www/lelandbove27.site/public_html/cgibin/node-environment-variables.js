fb6d6fd30e9044a50f07fb28dfa549d71a26db03
lbove27 <lbove@ucsd.edu>
2023-04-30 18:32:22 -0700

    node env vars

---
+++ www/lelandbove27.site/public_html/cgibin/node-environment-variables.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+process.stdout.write("Content-Type: text/html\r\n\r\n");
+process.stdout.write("<!DOCTYPE html>");
+process.stdout.write("<html lang='en'>");
+process.stdout.write("<head>");
+process.stdout.write("<title>Node Environment Variables</title>");
+process.stdout.write("</head>");
+process.stdout.write("<body>");
+process.stdout.write("<h1>Node Environment Variables</h1>");
+process.stdout.write("<hr>");
+process.stdout.write("<ul>");
+for(let property in process.env) {
+    process.stdout.write("<li><b>" + property + "</b>" +  process.env[property] + "</li>");
+}
+process.stdout.write("</ul>");
+process.stdout.write("</body>");
+process.stdout.write("</html>");
