9a98575c13a91d59ee7696a862d01c53baa9ab3b
lbove27 <lbove@ucsd.edu>
2023-05-12 18:41:59 -0700

    fixed endpoints

--- www/lelandbove27.site/public_html/endpoints.js
+++ www/lelandbove27.site/public_html/endpoints.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-app.get(port, () => console.log('testing if this works'));
+app.get("/", () => console.log('testing if this works'));
 
 app.listen(port, () => console.log('Listening on port ' + port));
