9955e31ec53036fb478f738787418680c3e19da4
lbove27 <lbove@ucsd.edu>
2023-05-13 09:08:58 -0700

    update endpoints

--- www/lelandbove27.site/public_html/endpoints.js
+++ www/lelandbove27.site/public_html/endpoints.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-app.get("/api", (req, res) => {
-    console.log('testing if this works');
+app.get("/", (req, res) => {
+    console.log('testing if this updated');
+    res.send("Hello world");
 });
 
 app.listen(port, () => console.log('Listening on port ' + port));
