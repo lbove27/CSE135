bd7535cce141de32ec3dd251551dfaeee7249916
lbove27 <lbove@ucsd.edu>
2023-05-16 16:01:32 -0700

    test window size

--- www/lelandbove27.site/public_html/collector.js
+++ www/lelandbove27.site/public_html/collector.js
@@ -4,5 +4,12 @@ window.onload = () => {
     let userAgent = navigator.userAgent;
     let language = navigator.language;
     let cookiesEnabled = navigator.cookieEnabled;
+    let jsEnabled = true;
+    //to do this, try seeing if there is some sort of displacement for a small image
+    //let imagesEnabled =
+    //try changing the style of something in css and then checking that attribute on load 
+    //let cssEnabled = 
+    let windowSize = window.screen;
+    console.log(window.screen);
 
 }
