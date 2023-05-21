20ba73651083f89fff09670a1c6c4cf7817a8253
lbove27 <lbove@ucsd.edu>
2023-05-21 16:58:29 -0700

    test mongodb

--- www/lelandbove27.site/public_html/collector.js
+++ www/lelandbove27.site/public_html/collector.js
@@ -1,5 +1,4 @@
-
-
+//static data / performance data
 window.onload = () => {
     let userAgent = navigator.userAgent;
     let language = navigator.language;
@@ -9,7 +8,67 @@ window.onload = () => {
     //let imagesEnabled =
     //try changing the style of something in css and then checking that attribute on load 
     //let cssEnabled = 
-    let windowSize = window.screen;
-    console.log(window.screen);
+    let screenHeight = window.screen.height;
+    let screenWidth = window.screen.width;
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
 
+    let staticData = {
+        "User Agent": userAgent,
+        "Language": language,
+        "Cookies Enabled": cookiesEnabled
+        //continue
+    }
+
+    //performance data
+    setTimeout(function() {
+        let timingObj = performance.getEntriesByType("navigation")[0];
+        let pageStart = timingObj["loadEventStart"];
+        let pageEnd = timingObj["loadEventEnd"];
+        let totalLoadTime = timingObj["loadEventEnd"] - timingObj["loadEventStart"];
+    }, 0);
 }
+
+//capture all other errors
+window.onerror = function (msg, url, lineNo, columnNo, error) {
+    console.log(msg);
+    console.log(url);
+    console.log(lineNo);
+    console.log(columnNo);
+    console.log(error);
+    alert('hi');
+}
+//capture HTTP errors
+window.addEventListener('error', function(event) {
+    console.log(event);
+}, true);
+
+
+//activity data
+
+//Mouse Movement
+window.addEventListener("mousemove", (event) => {
+    let x = event.screenX
+    let y = event.screenY
+});
+
+//Clicks
+window.addEventListener("scroll", (event) => {
+    const tScroll = document.body.scrollHeight - window.innerHeight;
+    console.log((window.pageYOffset * 100) / tScroll);
+});
+
+
+//Keyboard activity 
+window.addEventListener("keydown", (event) => {
+    console.log(event);
+    let key = event.key;
+});
+
+
+//Idle time
+
+
+
+
+
--- www/lelandbove27.site/public_html/endpoints.js
+++ www/lelandbove27.site/public_html/endpoints.js
@@ -1,3 +1,31 @@
+//MongoDB Stuff
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://lbove:tHpwlEOR0dxptTgt@cluster0.lbkxxfj.mongodb.net/?retryWrites=true&w=majority";
+
+//Test mongodb
+async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+//Express Endpoints 
 const express = require('express');
 const app = express();
 const port = 3001;
@@ -5,6 +33,10 @@ const port = 3001;
 app.get("/", (req, res) => {
     console.log('testing if this updated');
     res.send("Hello world");
+});
+
+app.post("/static", (req, res) => {
+    console.log('it worked');
 });
 
 app.listen(port, () => console.log('Listening on port ' + port));
