a3d276703c1f5cbdb0da6f853b3159a5b9e1dba2
lbove27 <lbove@ucsd.edu>
2023-06-04 16:22:38 -0700

    update routes

--- www/reporting.lelandbove27.site/public_html/userRoutes.js
+++ www/reporting.lelandbove27.site/public_html/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router();
-const User = require("./User");
+const User = require("./user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const auth = require("./auth");
