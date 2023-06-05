e0ed83058129feed9054c5a9fbed3cccca563b72
lbove27 <lbove@ucsd.edu>
2023-06-04 17:05:51 -0700

    fixed?

--- www/reporting.lelandbove27.site/public_html/userRoutes.js
+++ www/reporting.lelandbove27.site/public_html/userRoutes.js
@@ -1,9 +1,25 @@
 const express = require("express")
-const router = express.Router();
+//const router = express.Router();
 const User = require("./model/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const auth = require("./middleware/auth");
+
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const user = require("./userRoutes");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const MONGOURI = "mongodb+srv://lbove:tHpwlEOR0dxptTgt@cluster0.lbkxxfj.mongodb.net/?retryWrites=true&w=majority";
+
+
+app.use(bodyParser.json());
+app.use(cors())
+
+app.use("/user", user);
 
 router.post("/register", async(req,res) =>{
     const {username, email, password} = req.body;
@@ -81,4 +97,14 @@ router.get("/secure-api", auth, async (req, res) => {
     }
 });
 
-module.exports = router;
+mongoose.connect(MONGOURI)
+.then(() => {
+	console.log("Connected to DB");
+	app.listen(3002, () => {
+		console.log(`Node is running on port 3002`);
+    });
+  })
+  .catch((error) => {
+	console.log(error);
+  });
+//module.exports = router;
