20a9eb30426d346c578eaba54c8eac04fe7bfb74
lbove27 <lbove@ucsd.edu>
2023-06-06 14:52:51 -0700

    fixed failed login screen

--- www/reporting.lelandbove27.site/public_html/userRoutes.js
+++ www/reporting.lelandbove27.site/public_html/userRoutes.js
@@ -129,7 +129,7 @@ app.get("/dashboard/:authToken", async (req, res) => {
         if(!found) {
             res.status(404);
             res.header("Content-Type: text/html");
-            res.send("<html><body><h1>Go back to the login please</h1></body></html>");
+            res.send("<html><body><h1><a href=./login.html>Go back to the login please</a></h1></body></html>");
         }
         else {
             res.header("Content-Type: text/html");
