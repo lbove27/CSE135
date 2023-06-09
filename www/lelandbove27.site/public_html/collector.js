e4c1585a26448b25edcc1b184e33f7feb307de8b
lbove27 <lbove@ucsd.edu>
2023-06-08 21:56:11 -0700

    made id a string

--- www/lelandbove27.site/public_html/collector.js
+++ www/lelandbove27.site/public_html/collector.js
@@ -1,6 +1,8 @@
 //const crypto = window.crypto;
 //const sessionId = crypto.randomBytes(16).toString("hex");
-const sessionId = crypto.getRandomValues(new Uint8Array(2)).toString(16);
+const randomValues = crypto.getRandomValues(new Uint8Array(2));
+let sessionId = Array.from(randomValues).map(int => int.toString(16).padStart(2, '0')).join('');
+
 
 sessionStorage.setItem('sessionId', sessionId);
 
