5c87bfb77e51de72534d3f86cf7747d77660ef7e
lbove27 <lbove@ucsd.edu>
2023-06-10 09:04:25 -0700

    added license

--- www/reporting.lelandbove27.site/public_html/report.js
+++ www/reporting.lelandbove27.site/public_html/report.js
@@ -1,3 +1,5 @@
+ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "b55b025e438fa8a98e32482b5f768ff5"];
+
 async function createBarChart() {
     await fetch("/api/activity/getType/error", {
       method: "GET"
