cef2ba913e7b6364a55a301f5f7cb1dc2772828c
lbove27 <lbove@ucsd.edu>
2023-06-10 10:51:02 -0700

    test

--- www/reporting.lelandbove27.site/public_html/report.js
+++ www/reporting.lelandbove27.site/public_html/report.js
@@ -253,7 +253,184 @@ async function createBarChart() {
       
     });
 
-  }
+}
+
+async function createBrowserBarChart() {
+    var browserBarConfig = {
+        "graphset": [{
+          "type": "bar",
+          "background-color": "white",
+          "title": {
+            "text": "Tech Giant Quarterly Revenue",
+            "font-color": "#7E7E7E",
+            "backgroundColor": "none",
+            "font-size": "22px",
+            "alpha": 1,
+            "adjust-layout": true,
+          },
+          "plotarea": {
+            "margin": "dynamic"
+          },
+          "legend": {
+            "layout": "x3",
+            "overflow": "page",
+            "alpha": 0.05,
+            "shadow": false,
+            "align": "center",
+            "adjust-layout": true,
+            "marker": {
+              "type": "circle",
+              "border-color": "none",
+              "size": "10px"
+            },
+            "border-width": 0,
+            "maxItems": 3,
+            "toggle-action": "hide",
+            "pageOn": {
+              "backgroundColor": "#000",
+              "size": "10px",
+              "alpha": 0.65
+            },
+            "pageOff": {
+              "backgroundColor": "#7E7E7E",
+              "size": "10px",
+              "alpha": 0.65
+            },
+            "pageStatus": {
+              "color": "black"
+            }
+          },
+          "plot": {
+            "bars-space-left": 0.15,
+            "bars-space-right": 0.15,
+            "animation": {
+              "effect": "ANIMATION_SLIDE_BOTTOM",
+              "sequence": 0,
+              "speed": 800,
+              "delay": 800
+            }
+          },
+          "scale-y": {
+            "line-color": "#7E7E7E",
+            "item": {
+              "font-color": "#7e7e7e"
+            },
+            "values": "0:60:10",
+            "guide": {
+              "visible": true
+            },
+            "label": {
+              "text": "$ Billions",
+              "font-family": "arial",
+              "bold": true,
+              "font-size": "14px",
+              "font-color": "#7E7E7E",
+            },
+          },
+          "scaleX": {
+            "values": [
+              "Browsers"
+            ],
+            "placement": "default",
+            "tick": {
+              "size": 58,
+              "placement": "cross"
+            },
+            "itemsOverlap": true,
+            "item": {
+              "offsetY": -55
+            }
+          },
+          "tooltip": {
+            "visible": false
+          },
+          "crosshair-x": {
+            "line-width": "100%",
+            "alpha": 0.18,
+            "plot-label": {
+              "header-text": "%kv Sales"
+            }
+          },
+          "series": [{
+              "values": [
+                37.47,
+              ],
+              "alpha": 0.95,
+              "borderRadiusTopLeft": 7,
+              "background-color": "purple",
+              "text": "Apple",
+            },
+            {
+              "values": [
+                2.02,
+                2.59,
+                2.5,
+                2.91
+              ],
+              "borderRadiusTopLeft": 7,
+              "alpha": 0.95,
+              "background-color": "orange",
+              "text": "Facebook"
+            },
+            {
+              "values": [
+                13.4,
+                14.11,
+                14.89,
+                16.86
+              ],
+              "alpha": 0.95,
+              "borderRadiusTopLeft": 7,
+              "background-color": "teal",
+              "text": "Google"
+            },
+            {
+              "values": [
+                18.53,
+                24.52,
+                20.4,
+                23.38
+              ],
+              "borderRadiusTopLeft": 7,
+              "alpha": 0.95,
+              "background-color": "red",
+              "text": "Microsoft"
+            },
+            {
+              "values": [
+                17.09,
+                25.59,
+                19.74,
+                19.34
+              ],
+              "borderRadiusTopLeft": 7,
+              "alpha": 0.95,
+              "background-color": "blue",
+              "text": "Amazon"
+            },
+            {
+              "values": [
+                2.31,
+                2.36,
+                2.42,
+                2.52
+              ],
+              "borderRadiusTopLeft": 7,
+              "alpha": 0.95,
+              "background-color": "green",
+              "text": "Cognizant"
+            }
+          ]
+        }]
+      };
+   
+      zingchart.render({
+        id: 'browserBarChart',
+        data: browserBarConfig,
+        height: '100%',
+        width: '100%'
+      });
+}
 
 
   async function createAllMetrics() {
@@ -261,7 +438,7 @@ async function createBarChart() {
       await createBarChart();
     }, 1000);
     setTimeout(async () => {
-     
+      await createBrowserBarChart();
     }, 1500);
     setTimeout(async () => {
       
