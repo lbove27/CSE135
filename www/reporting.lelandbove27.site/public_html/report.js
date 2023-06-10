85ba8c7e100c48741a6575eb0d3ee77bd6fd8e6f
lbove27 <lbove@ucsd.edu>
2023-06-10 11:07:17 -0700

    fill with data

--- www/reporting.lelandbove27.site/public_html/report.js
+++ www/reporting.lelandbove27.site/public_html/report.js
@@ -256,145 +256,174 @@ async function createBarChart() {
 }
 
 async function createBrowserBarChart() {
-    var browserBarConfig = {
-        "graphset": [{
-          "type": "bar",
-          "background-color": "white",
-          "title": {
-            "text": "Users Per Browser",
-            "font-color": "#000000",
-            "backgroundColor": "none",
-            "font-size": "22px",
-            "alpha": 1,
-            "adjust-layout": true,
-          },
-          "plotarea": {
-            "margin": "dynamic"
-          },
-          "legend": {
-            "alpha": 0.05,
-            "shadow": false,
-            "align": "center",
-            "adjust-layout": true,
-            "marker": {
-              "type": "square",
-              "border-color": "none",
-              "size": "10px"
-            },
-            "border-width": 0,
-            "maxItems": 3,
-            "toggle-action": "hide",
-            "pageOn": {
-              "backgroundColor": "#000",
-              "size": "10px",
-              "alpha": 0.65
-            },
-            "pageOff": {
-              "backgroundColor": "#7E7E7E",
-              "size": "10px",
-              "alpha": 0.65
-            },
-            "pageStatus": {
-              "color": "black"
-            }
-          },
-          "plot": {
-            "bars-space-left": 0.15,
-            "bars-space-right": 0.15,
-            "animation": {
-              "effect": "ANIMATION_SLIDE_BOTTOM",
-              "sequence": 0,
-              "speed": 800,
-              "delay": 800
-            }
-          },
-          "scale-y": {
-            "line-color": "#7E7E7E",
-            "angle": "90",
-            "item": {
-              "font-color": "#7e7e7e"
-            },
-            "values": "0:60:10",
-            "guide": {
-              "visible": true
-            },
-            "label": {
-              "text": "Users",
-              "font-family": "arial",
-              "bold": true,
-              "font-size": "14px",
-              "font-color": "#7E7E7E",
-            },
-          },
-          "scaleX": {
-            "values": [
-              "Browsers"
-            ],
-            "placement": "default",
-            "tick": {
-              "size": 58,
-              "placement": "cross"
-            },
-            "itemsOverlap": true,
-            "item": {
-              "offsetY": -55
-            }
-          },
-          "tooltip": {
-            "visible": false
-          },
-          "crosshair-x": {
-            "line-width": "100%",
-            "alpha": 0.18,
-            "plot-label": {
-              "header-text": "%kv Sales"
-            }
-          },
-          "series": [{
-              "values": [
-                37.47
-              ],
-              "background-color": "#FDD20A",
-              "text": "Chrome",
-            },
-            {
-              "values": [
-                2.02
-              ],
-              "background-color": "#006CFF",
-              "text": "Safari"
-            },
-            {
-                "values": [
-                  2.02
-                ],
-                "background-color": "#55D555",
-                "text": "Edge"
+    await fetch("https://lelandbove27.site/api/static", {
+        method: "GET"
+      }).then(response => response.json()).then(data => {
+        let staticData = data;
+        
+        let chrome = 0;
+        let firefox = 0;
+        let edge = 0;
+        let safari = 0;
+        let other = 0;
+        staticData.forEach(obj => {
+          let userAgent = obj["User Agent"];
+          let chromeBool = userAgent.includes('Chrome');
+          let edgeBool = userAgent.includes('Edg');
+          let firefoxBool = userAgent.includes('Fx') || userAgent.includes('Firefox');
+          let safariBool = userAgent.includes('Safari') && !userAgent.includes('Chrome');
+          if(chromeBool && !firefoxBool && !edgeBool) {
+            chrome++;
+          }
+          else if(firefoxBool) {
+            firefox++;
+          }
+          else if(edgeBool) {
+            edge++;
+          }
+          else if(safariBool) {
+            safari++;
+          }
+          else {
+            other++;
+          }
+
+          var browserBarConfig = {
+            "graphset": [{
+              "type": "bar",
+              "background-color": "white",
+              "title": {
+                "text": "Users Per Browser",
+                "font-color": "#000000",
+                "backgroundColor": "none",
+                "font-size": "22px",
+                "alpha": 1,
+                "adjust-layout": true,
               },
-            {
-              "values": [
-                13.4
-              ],
-              "background-color": "#E66000",
-              "text": "Firefox"
-            },
-            {
-              "values": [
-                18.53,
-              ],
-              "background-color": "#D3D3D3",
-              "text": "Other"
-            },
-          ]
-        }]
-      };
-   
-      zingchart.render({
-        id: 'browserBarChart',
-        data: browserBarConfig,
-        height: '100%',
-        width: '100%'
-      });
+              "plotarea": {
+                "margin": "dynamic"
+              },
+              "legend": {
+                "alpha": 0.05,
+                "shadow": false,
+                "align": "center",
+                "adjust-layout": true,
+                "marker": {
+                  "type": "square",
+                  "border-color": "none",
+                  "size": "10px"
+                },
+                "border-width": 0,
+                "maxItems": 3,
+                "toggle-action": "hide",
+                "pageOn": {
+                  "backgroundColor": "#000",
+                  "size": "10px",
+                  "alpha": 0.65
+                },
+                "pageOff": {
+                  "backgroundColor": "#7E7E7E",
+                  "size": "10px",
+                  "alpha": 0.65
+                },
+                "pageStatus": {
+                  "color": "black"
+                }
+              },
+              "plot": {
+                "bars-space-left": 0.15,
+                "bars-space-right": 0.15,
+                "animation": {
+                  "effect": "ANIMATION_SLIDE_BOTTOM",
+                  "sequence": 0,
+                  "speed": 800,
+                  "delay": 800
+                }
+              },
+              "scale-y": {
+                "line-color": "#7E7E7E",
+                "item": {
+                  "font-color": "#7e7e7e"
+                },
+                "values": "0:60:10",
+                "guide": {
+                  "visible": true
+                },
+                "label": {
+                  "text": "Users",
+                  "font-family": "arial",
+                  "bold": true,
+                  "font-size": "14px",
+                  "font-color": "#7E7E7E",
+                },
+              },
+              "scaleX": {
+                "values": [
+                  "Browsers"
+                ],
+                "placement": "default",
+                "tick": {
+                  "size": 58,
+                  "placement": "cross"
+                },
+                "itemsOverlap": true,
+                "item": {
+                  "offsetY": -55
+                }
+              },
+              "tooltip": {
+                "visible": false
+              },
+              "series": [{
+                  "values": [
+                    chrome
+                  ],
+                  "background-color": "#FDD20A",
+                  "text": "Chrome",
+                },
+                {
+                  "values": [
+                    safari
+                  ],
+                  "background-color": "#006CFF",
+                  "text": "Safari"
+                },
+                {
+                    "values": [
+                      edge
+                    ],
+                    "background-color": "#55D555",
+                    "text": "Edge"
+                  },
+                {
+                  "values": [
+                    firefox
+                  ],
+                  "background-color": "#E66000",
+                  "text": "Firefox"
+                },
+                {
+                  "values": [
+                    other
+                  ],
+                  "background-color": "#D3D3D3",
+                  "text": "Other"
+                },
+              ]
+            }]
+          };
+       
+          zingchart.render({
+            id: 'browserBarChart',
+            data: browserBarConfig,
+            height: '100%',
+            width: '100%'
+          });
+
+
+
+        });
+    });
 }
 
 
