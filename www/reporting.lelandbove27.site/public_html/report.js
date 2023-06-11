bd2d0fe75787fa2c5b1b988c4bb0d7686c8d9779
lbove27 <lbove@ucsd.edu>
2023-06-10 21:24:38 -0700

    update report

--- www/reporting.lelandbove27.site/public_html/report.js
+++ www/reporting.lelandbove27.site/public_html/report.js
@@ -1,5 +1,38 @@
 ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "b55b025e438fa8a98e32482b5f768ff5"];
 
+let dashboard = document.getElementById('dashboard-link');
+dashboard.href = "https://reporting.lelandbove27.site/user/dashboard/" + sessionStorage.getItem('auth_token');
+
+fetch("/user/adminAccess/" + sessionStorage.getItem('auth_token'), {
+  method: "GET"
+}).then(response => response.json()).then(data => {
+  if(data.adminBool == 'true') {
+    let nav = document.getElementById('navbar');
+    let link = document.createElement('a');
+    link.innerHTML = "User Grid";
+    link.href = "https://reporting.lelandbove27.site/user/users/" + sessionStorage.getItem('auth_token'); 
+    nav.appendChild(link);
+  }
+});
+
+async function createErrorsToday() {
+  await fetch("https://lelandbove27.site/api/activity/getType/error", {
+        method: "GET"
+    }).then(response => response.json()).then(data => {
+        let totalCount = 0;
+        let date = new Date();
+        let stringDate = date.toISOString();
+        let todayDate = stringDate.split('T')[0];
+        data.forEach(obj => {
+            if(obj.date.includes(todayDate)) {
+              totalCount++;
+            }
+        });
+        let errorsToday = document.getElementById('errors-today');
+        errorsToday.innerHTML = totalCount;
+    });
+}
+
 async function createTotalErrors() {
     await fetch("https://lelandbove27.site/api/activity/getType/error", {
         method: "GET"
@@ -108,7 +141,7 @@ async function createBarChart() {
               "text": "Number of Errors Based on Browser",
               "font-color": "#000000",
               "backgroundColor": "white",
-              "font-size": "35px",
+              "font-size": "40px",
               "alpha": 1,
               "adjust-layout": true,
               "font-family": "Crimson Text"
@@ -237,173 +270,174 @@ async function createBarChart() {
 }
 
 async function createBrowserBarChart() {
-    await fetch("https://lelandbove27.site/api/static", {
-        method: "GET"
-      }).then(response => response.json()).then(data => {
-        let staticData = data;
-        
-        let chrome = 0;
-        let firefox = 0;
-        let edge = 0;
-        let safari = 0;
-        let other = 0;
-        staticData.forEach(obj => {
-          let userAgent = obj["User Agent"];
-          let chromeBool = userAgent.includes('Chrome');
-          let edgeBool = userAgent.includes('Edg');
-          let firefoxBool = userAgent.includes('Fx') || userAgent.includes('Firefox');
-          let safariBool = userAgent.includes('Safari') && !userAgent.includes('Chrome');
-          if(chromeBool && !firefoxBool && !edgeBool) {
-            chrome++;
-          }
-          else if(firefoxBool) {
-            firefox++;
-          }
-          else if(edgeBool) {
-            edge++;
-          }
-          else if(safariBool) {
-            safari++;
-          }
-          else {
-            other++;
-          }
+  await fetch("https://lelandbove27.site/api/static", {
+      method: "GET"
+    }).then(response => response.json()).then(data => {
+      let staticData = data;
+      
+      let chrome = 0;
+      let firefox = 0;
+      let edge = 0;
+      let safari = 0;
+      let other = 0;
+      staticData.forEach(obj => {
+        let userAgent = obj["User Agent"];
+        let chromeBool = userAgent.includes('Chrome');
+        let edgeBool = userAgent.includes('Edg');
+        let firefoxBool = userAgent.includes('Fx') || userAgent.includes('Firefox');
+        let safariBool = userAgent.includes('Safari') && !userAgent.includes('Chrome');
+        if(chromeBool && !firefoxBool && !edgeBool) {
+          chrome++;
+        }
+        else if(firefoxBool) {
+          firefox++;
+        }
+        else if(edgeBool) {
+          edge++;
+        }
+        else if(safariBool) {
+          safari++;
+        }
+        else {
+          other++;
+        }
 
 
-          var browserBarConfig = {
-            "graphset": [{
-              "type": "bar",
-              "background-color": "white",
-              "title": {
-                "text": "Users Per Browser",
-                "font-color": "#000000",
-                "backgroundColor": "none",
-                "font-size": "22px",
-                "alpha": 1,
-                "adjust-layout": true,
+        var browserBarConfig = {
+          "graphset": [{
+            "type": "bar",
+            "background-color": "white",
+            "title": {
+              "text": "Users Per Browser",
+              "font-color": "#000000",
+              "backgroundColor": "none",
+              "font-size": "22px",
+              "alpha": 1,
+              "adjust-layout": true,
+            },
+            "plotarea": {
+              "margin": "dynamic"
+            },
+            "legend": {
+              "alpha": 0.05,
+              "shadow": false,
+              "align": "center",
+              "adjust-layout": true,
+              "marker": {
+                "type": "square",
+                "border-color": "none",
+                "size": "10px"
               },
-              "plotarea": {
-                "margin": "dynamic"
+              "border-width": 0,
+              "maxItems": 3,
+              "toggle-action": "hide",
+              "pageOn": {
+                "backgroundColor": "#000",
+                "size": "10px",
+                "alpha": 0.65
               },
-              "legend": {
-                "alpha": 0.05,
-                "shadow": false,
-                "align": "center",
-                "adjust-layout": true,
-                "marker": {
-                  "type": "square",
-                  "border-color": "none",
-                  "size": "10px"
-                },
-                "border-width": 0,
-                "maxItems": 3,
-                "toggle-action": "hide",
-                "pageOn": {
-                  "backgroundColor": "#000",
-                  "size": "10px",
-                  "alpha": 0.65
-                },
-                "pageOff": {
-                  "backgroundColor": "#7E7E7E",
-                  "size": "10px",
-                  "alpha": 0.65
-                },
-                "pageStatus": {
-                  "color": "black"
-                }
+              "pageOff": {
+                "backgroundColor": "#7E7E7E",
+                "size": "10px",
+                "alpha": 0.65
               },
-              "plot": {
-                "bars-space-left": 0.15,
-                "bars-space-right": 0.15,
-                "animation": {
-                  "effect": "ANIMATION_SLIDE_BOTTOM",
-                  "sequence": 0,
-                  "speed": 800,
-                  "delay": 800
-                }
+              "pageStatus": {
+                "color": "black"
+              }
+            },
+            "plot": {
+              "bars-space-left": 0.15,
+              "bars-space-right": 0.15,
+              "animation": {
+                "effect": "ANIMATION_SLIDE_BOTTOM",
+                "sequence": 0,
+                "speed": 800,
+                "delay": 800
+              }
+            },
+            "scale-y": {
+              "line-color": "#7E7E7E",
+              "item": {
+                "font-color": "#7e7e7e"
               },
-              "scale-y": {
-                "line-color": "#7E7E7E",
-                "item": {
-                  "font-color": "#7e7e7e"
-                },
-                "values": "0:" + (chrome * 1.4) + ":5",
-                "guide": {
-                  "visible": true
-                },
-                "label": {
-                  "text": "Users",
-                  "font-family": "arial",
-                  "bold": true,
-                  "font-size": "14px",
-                  "font-color": "#7E7E7E",
-                },
+              "values": "0:" + (chrome * 1.4) + ":5",
+              "guide": {
+                "visible": true
               },
-              "scaleX": {
+              "label": {
+                "text": "Users",
+                "font-family": "arial",
+                "bold": true,
+                "font-size": "14px",
+                "font-color": "#7E7E7E",
+              },
+            },
+            "scaleX": {
+              "values": [
+                "Browsers"
+              ],
+              "placement": "default",
+              "tick": {
+                "size": 58,
+                "placement": "cross"
+              },
+              "itemsOverlap": true,
+              "item": {
+                "offsetY": -55
+              }
+            },
+            "tooltip": {
+              "text": "Chrome: " + chrome + "\nSafari: " + safari + "\nEdge: " + edge + "\nFirefox: " + firefox + "\nOther: " + other,
+            },
+            "series": [{
                 "values": [
-                  "Browsers"
+                  chrome
                 ],
-                "placement": "default",
-                "tick": {
-                  "size": 58,
-                  "placement": "cross"
-                },
-                "itemsOverlap": true,
-                "item": {
-                  "offsetY": -55
-                }
+                "background-color": "#FDD20A",
+                "label": "Chrome",
+                "text": "Chrome"
               },
-              "tooltip": {
-                "visible": false
+              {
+                "values": [
+                  safari
+                ],
+                "background-color": "#006CFF",
+                "text": "Safari"
               },
-              "series": [{
+              {
                   "values": [
-                    chrome
+                    edge
                   ],
-                  "background-color": "#FDD20A",
-                  "text": "Chrome",
+                  "background-color": "#55D555",
+                  "text": "Edge"
                 },
-                {
-                  "values": [
-                    safari
-                  ],
-                  "background-color": "#006CFF",
-                  "text": "Safari"
-                },
-                {
-                    "values": [
-                      edge
-                    ],
-                    "background-color": "#55D555",
-                    "text": "Edge"
-                  },
-                {
-                  "values": [
-                    firefox
-                  ],
-                  "background-color": "#E66000",
-                  "text": "Firefox"
-                },
-                {
-                  "values": [
-                    other
-                  ],
-                  "background-color": "#D3D3D3",
-                  "text": "Other"
-                },
-              ]
-            }]
-          };
-       
-          zingchart.render({
-            id: 'browserBarChart',
-            data: browserBarConfig,
-            height: '100%',
-            width: '100%'
-          });
-
+              {
+                "values": [
+                  firefox
+                ],
+                "background-color": "#E66000",
+                "text": "Firefox"
+              },
+              {
+                "values": [
+                  other
+                ],
+                "background-color": "#D3D3D3",
+                "text": "Other"
+              },
+            ]
+          }]
+        };
+     
+        zingchart.render({
+          id: 'browserBarChart',
+          data: browserBarConfig,
+          height: '100%',
+          width: '100%'
         });
-    });
+
+      });
+  });
 }
 
 
@@ -417,6 +451,7 @@ async function createBrowserBarChart() {
     setTimeout(async () => {
       await createBrowserBarChart();
     }, 0);
+    await createErrorsToday();
   }
     
   
